fd561fc4821cf5e5bf919cedd0d0e6b4c80f766a
Ben <Blorantfy-cc@conestogac.on.ca>
2016-10-31 14:42:06 -0400

    markup

--- output/safari/chatterbox_0.9.0.safariextension/content.js
+++ output/safari/chatterbox_0.9.0.safariextension/content.js
@@ -4,16 +4,11 @@
 // ==/UserScript==
 
 (function($,window,document){
-
-	var colorNumber = 0;
-	var colors = ['pink', 'yellow', 'lightblue'];
-
-	window.setInterval(function() {
-		$("body").css("background",colors[colorNumber++]);
-		if(colorNumber > colors.length) {
-			colorNumber = 0;
-		}
-	}, 1000);
+	// [ Get's comment box markup ]
+	var url = kango.io.getResourceUrl('res/box.html');
+	$.get(url,function(box){
+		$("body").append(box);
+	});
 	
 })(__chaterbox_jquery__,window,document);
 
